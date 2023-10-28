dcd4181a4f4861fff133441278ab6158462690a1
IsadoraPassos <104655270+IsadoraPassos@users.noreply.github.com>
2023-10-28 19:35:07 -0300

    Update filtro.py para usar o silenium

--- filtro.py
+++ filtro.py
@@ -1,4 +1,4 @@
-import requests
+from selenium import webdriver
 
 # Lista para armazenar sites acessíveis
 sites_acessiveis = []
@@ -7,22 +7,27 @@ sites_acessiveis = []
 with open('list_browser.txt', 'r') as file:
     sites = file.readlines()
 
-# Iterar sobre cada site na lista
+# Inicializar o WebDriver do navegador (ex: Chrome)
+driver = webdriver.Chrome()  # Certifique-se de ter o ChromeDriver configurado
+
 for site in sites:
     url = site.strip()  # Remove espaços em branco ou caracteres de nova linha
 
     try:
-        response = requests.get(url)
-        # Verifica se o status do código é 200 (OK) e se não contém a mensagem de erro
-        if response.status_code == 200 and "Não é possível acessar esse site" not in response.text:
+        driver.get(url)  # Abre o site no navegador
+        # Verifica se a página contém a mensagem de erro
+        if "Não é possível acessar esse site" not in driver.page_source:
             sites_acessiveis.append(url)
             print(f'O site {url} está acessível.')
         else:
             print(f'O site {url} não está acessível ou exibe a mensagem de erro.')
-    except requests.RequestException as e:
+    except Exception as e:
         print(f'O site {url} não está acessível. Erro: {e}')
 
-# Criar um novo arquivo com os sites acessíveis
+# Encerra o WebDriver
+driver.quit()
+
+# Cria um novo arquivo com os sites acessíveis
 with open('sites_acessiveis.txt', 'w') as output_file:
     for site in sites_acessiveis:
         output_file.write(site + '\n')
