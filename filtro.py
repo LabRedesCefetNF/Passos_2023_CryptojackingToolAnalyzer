44e22a2b34acba0ebf90b7609bb1a04f623839cb
IsadoraPassos <104655270+IsadoraPassos@users.noreply.github.com>
2023-10-28 20:16:27 -0300

    Update filtro.py

--- filtro.py
+++ filtro.py
@@ -12,7 +12,7 @@ for site in sites:
         url = 'https://' + url  # Adiciona "https://" se não estiver presente
 
     try:
-        response = requests.get(url)
+        response = requests.get(url, timeout=10)  # Define um tempo limite de 10 segundos para a requisição
         if response.status_code == 200:
             sites_acessiveis.append(url)
             print(f'O site {url} está acessível.')
@@ -20,6 +20,8 @@ for site in sites:
             print(f'O site {url} não está acessível. Código de status: {response.status_code}')
     except requests.RequestException as e:
         print(f'O site {url} não está acessível. Erro: {e}')
+    except Exception as ex:
+        print(f'Um erro inesperado ocorreu para o site {url}. Erro: {ex}')
 
 with open('sites_acessiveis.txt', 'w') as output_file:
     for site in sites_acessiveis:
